d8498dcde81ba2459adc6acb67dc57eeabc55e3f
waterspamer <volard39@gmail.com>
2019-04-30 15:37:21 +0300

    Changes

--- Assets/Scripts/MenuTurretScript.cs
+++ Assets/Scripts/MenuTurretScript.cs
@@ -18,7 +18,7 @@ public class MenuTurretScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayText.GetComponent<Text>().text = "";
+        //PlayText.GetComponent<Text>().text = "";
     }
 
     // Update is called once per frame
